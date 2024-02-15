d50c584dcc96b4b73e516769955e2e463ae7a64e
SamuelArdi <fiicery.work@gmail.com>
2024-02-15 21:16:41 +0800

    fixed navbar bug and added new featurse
    Fixed the bug with the navbar not hiding well, and added buttons to show and hid e the links

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const navBarShades = document.querySelector(".navBarShades");
 
 const title = document.querySelector(".title");
 const hamburgerBtn = document.querySelectorAll(".navButton");
+const hideLinksBtn = document.querySelector(".hideLinks");
+const showlinksBtn = document.querySelector(".showLinks");
+
+var linksHidden;
+var shade_1 = navBarShades.children[0];
+var shade_2 = navBarShades.children[1];
 
 function toggleNav(status) {
 
-    let shade_1 = navBarShades.children[0];
-    let shade_2 = navBarShades.children[1];
-
     if (status === "inactive") {
 
+        showlinksBtn.style.transform = "translateX(0)";
 
+        if (linksHidden === true) {
+            shade_2.style.visibility = "visible";
+        }
 
         navBar.style.transform = "translateY(-100%)";
         navBarShades.style.visibility = "visible";
@@ -21,6 +28,8 @@ function toggleNav(status) {
         shade_2.style.transform = "translateX(0)"
 
     } else if (status === "active") {
+
+        showlinksBtn.style.transform = "translateX(-100%)";
 
         navBar.style.transform = "translateY(0)";
         navBarShades.style.visibility = "hidden";
@@ -37,4 +46,22 @@ hamburgerBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
         toggleNav(btn.dataset.status);
     })
+})
+
+hideLinksBtn.addEventListener("click", () => {
+    
+    shade_2.style.transform = "translateX(-100%)"
+    shade_2.style.visibility = "hidden";
+
+    linksHidden = true;
+
+})
+
+showlinksBtn.addEventListener("click", () => {
+    
+    shade_2.style.transform = "translateX(0)"
+    shade_2.style.visibility = "visible";
+
+    linksHidden = false;
+
 })
